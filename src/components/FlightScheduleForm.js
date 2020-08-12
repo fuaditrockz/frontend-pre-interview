a139987a8736f56d8c6224f9a16a336d3c3c5294
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-12 23:38:17 +0700

    [FlightScheduleForm] Implement input component.

--- src/components/FlightScheduleForm.js
+++ src/components/FlightScheduleForm.js
@@ -1,12 +1,44 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet
+} from 'react-native'
+
+import Input from './Input'
 
 export default function FlightScheduleForm({}) {
   return (
     <View>
-      <Text>
-        Hello Flight!
-      </Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>
+          Set Flight Reminder
+        </Text>
+      </View>
+      <Input
+        placeholder='Your Flight Number'
+        onChangeText={t => console.log(t)}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    marginBottom: 20
+  },
+  title: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 20,
+    color: '#fff'
+  },
+  textInput: {
+    height: 40,
+    borderColor: '#d2dae2',
+    borderBottomWidth: 0.5,
+    fontFamily: 'Poppins-Regular',
+    paddingHorizontal: 10,
+    color: '#fff'
+  }
+})
