75052cebbc69ead69b18655f0df65ad96c3a63e3
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-12 22:42:06 +0700

    [src/index] Setup react-native-linear-gradient.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyleSheet, View, StatusBar } from 'react-native'
 import { NativeRouter, Route } from 'react-router-native'
+import LinearGradient from 'react-native-linear-gradient'
 
 import { RootContextProvider, RootContextConsumer } from './context'
 import SetupFlighScheduleScreen  from './screens/SetupFlightScheduleScreen'
@@ -9,20 +10,20 @@ export default function App() {
   return (
     <NativeRouter>
       <RootContextProvider>
-        <View style={styles.container}>
-          <RootContextConsumer>
-            {context => (
-              <StatusBar
-                backgroundColor={context.theme.backgroundColor}
-                barStyle={context.theme.statusBar}
-              />
-            )}
-          </RootContextConsumer>
+        <LinearGradient colors={['#3c40c6', '#575fcf']} style={styles.container}>
+            <RootContextConsumer>
+              {context => (
+                <StatusBar
+                  backgroundColor={context.theme.backgroundColor}
+                  barStyle={context.theme.statusBar}
+                />
+              )}
+            </RootContextConsumer>
 
-          <Route exact path="/" component={SetupFlighScheduleScreen} />
-          {/* <Route path="/about" component={About} />
-          <Route path="/topics" component={Topics} /> */}
-        </View>
+            <Route exact path="/" component={SetupFlighScheduleScreen} />
+            {/* <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} /> */}
+        </LinearGradient>
       </RootContextProvider>
     </NativeRouter>
   )
