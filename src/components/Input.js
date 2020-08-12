94654eaa96f4a8d4be592b206dcff99249087ba5
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-13 02:13:50 +0700

    [components/Input] Fixing animated with useNativeDriver.

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,23 +3,20 @@ import { View, Text, TextInput, StyleSheet, Animated } from 'react-native'
 
 export default function Input({ placeholder, label, onChangeText, ref }) {
   const [isInputActive, setIsInputActive] = useState(false)
-  const labelTopRef = useRef(new Animated.Value(10)).current
-  const labelLeftRef = useRef(new Animated.Value(10)).current
-  const labelFontSizeRef = useRef(new Animated.Value(14)).current
+  const labelPositionRef = useRef(new Animated.ValueXY({ x: 10, y: 10 })).current
+  const labelFontSizeRef = useRef(new Animated.Value(1)).current
   
   const moveLabel = (statusPosition) => {
     Animated.parallel([
-      Animated.timing(labelTopRef, {
-        toValue: statusPosition === 'move' ? -10 : 10,
-        duration: 300
-      }),
-      Animated.timing(labelLeftRef, {
-        toValue: statusPosition === 'move' ? 0 : 10,
-        duration: 300
+      Animated.timing(labelPositionRef, {
+        toValue: statusPosition === 'move' ? { x: 10, y: -10 } : { x: 10, y: 10 },
+        duration: 300,
+        useNativeDriver: true
       }),
       Animated.timing(labelFontSizeRef, {
-        toValue: statusPosition === 'move' ? 10 : 14,
-        duration: 300
+        toValue: statusPosition === 'move' ? 1 : 1,
+        duration: 300,
+        useNativeDriver: true
       })
     ]).start()
   }
@@ -41,11 +38,10 @@ export default function Input({ placeholder, label, onChangeText, ref }) {
       <Animated.View
         style={{
           position: 'absolute',
-          top: labelTopRef,
-          left: labelLeftRef
+          transform: [{ translateX: labelPositionRef.x }, { translateY: labelPositionRef.y }]
         }}
       >
-        <Animated.Text style={[ styles.label, { fontSize: labelFontSizeRef }]}>
+        <Animated.Text style={[ styles.label, { transform: [{ scale: labelFontSizeRef }] }]}>
           {label}
         </Animated.Text>
       </Animated.View>
@@ -54,7 +50,7 @@ export default function Input({ placeholder, label, onChangeText, ref }) {
         onChangeText={onChangeText}
         /* value={100} */
         placeholder={!isInputActive ? '' : placeholder}
-        placeholderTextColor='#d2dae2'
+        placeholderTextColor='#C5CEE1'
         onFocus={activateInput}
         onEndEditing={onEndEditing}
       />
@@ -71,11 +67,13 @@ const styles = StyleSheet.create({
     borderColor: '#d2dae2',
     borderBottomWidth: 0.5,
     fontFamily: 'Poppins-Regular',
+    fontSize: 16,
     paddingHorizontal: 10,
+    paddingTop: 5,
     color: '#fff'
   },
   label: {
     color: '#d2dae2',
-    fontFamily: 'Poppins-Medium'
+    fontFamily: 'Poppins-Bold'
   }
 })
