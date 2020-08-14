8f1cf199c8440ac531359456bcad0c5d91056fda
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-14 16:44:07 +0700

    [components/FlightScheduleForm] Matching local state data with flight API.

--- src/components/FlightScheduleForm.js
+++ src/components/FlightScheduleForm.js
@@ -7,12 +7,15 @@ import {
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5'
 
+import { formatDate } from '../helpers'
 import { RootContext, RootContextConsumer } from '../context'
 import { Input, Button } from './atoms'
 
 export default function FlightScheduleForm({}) {
+  const { airlines, saveFlight } = useContext(RootContext)
   const [flightNumber, setFlightNumber] = useState('')
   const [flightDate, setFlightDate] = useState(new Date())
+  const [checkedData, setCheckedData] = useState([])
 
   const onChangeFlightNumber = flNumber => {
     setFlightNumber(flNumber)
@@ -25,6 +28,52 @@ export default function FlightScheduleForm({}) {
   const eraseAllStateValues = () => {
     setFlightDate(new Date())
     setFlightNumber('')
+  }
+
+  const getDataFromFlightAPI = async (date) => {
+    const url = `https://www.hongkongairport.com/flightinfo-rest/rest/flights/past?date=${formatDate(date.toDateString())}&lang=en&cargo=false&arrival=false`
+
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      getFlightDetails(flightNumber, data[0].list)
+      console.log('getDataFromFlightAPI', data[0].list)
+      return data[0].list
+    }
+    catch (err) {
+      return console.log(err)
+    }
+  }
+
+  const getFlightDetails = (flNumber, flDatas) => {
+    const result = flDatas.filter((item) => {
+        const flNumberIndex = item.flight.map(fl => fl.no).indexOf(flNumber)
+        return !flNumberIndex
+    })
+  
+    console.log('getFlightDetails', result)
+    saveFlight({
+      ...result[0],
+      flightNumber,
+      flightDate,
+      isActive: true
+    })
+    return result
+  }
+
+  const setReminder = async () => {
+    const airlinesList = airlines[0].data
+    try {
+      getDataFromFlightAPI(flightDate)
+      eraseAllStateValues()
+    } catch (err) {
+      console.log(err)
+    }
+    /* saveFlight({
+      flightNumber,
+      flightDate,
+      isActive: true
+    }) */
   }
 
   return (
@@ -51,23 +100,13 @@ export default function FlightScheduleForm({}) {
       >
         <MaterialIcon name="date-range" size={15} color='#fff' />
       </Input>
-      <RootContextConsumer>
-        {context => (
-          <Button
-            title='SET REMINDER'
-            isHaveIcon
-            iconName='notifications'
-            onPressButton={() => {
-              context.saveFlight({
-                flightNumber,
-                flightDate
-              })
-              eraseAllStateValues()
-            }}
-            isDisabled={false}
-          />
-        )}
-      </RootContextConsumer>
+      <Button
+        title='SET REMINDER'
+        isHaveIcon
+        iconName='notifications'
+        onPressButton={setReminder}
+        isDisabled={false}
+      />
     </View>
   )
 }
