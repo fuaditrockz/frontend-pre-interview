c3c614ee7a1b24f86ff977c30c807d6f340dedf4
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-15 20:34:19 +0700

    [components/FlightScheduleForm] Delete

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,5 +1,4 @@
 export { default as SchedulesList } from './SchedulesList'
-export { default as FlightScheduleForm } from './FlightScheduleForm'
 export { default as SetReminderForm } from './SetReminderForm'
 export { default as Header } from './Header'
 export { default as SlidingPanel } from './SlidingPanel'
