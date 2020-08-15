9d5f891b6ef5cbe073ccff338b54f2699997cc6f
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-15 20:32:10 +0700

    components/SetReminderForm] Fixed matched data and storing.

--- src/components/organisms/SetReminderForm.js
+++ src/components/organisms/SetReminderForm.js
@@ -3,8 +3,7 @@ import {
   View,
   Text,
   StyleSheet,
-  Image,
-  ActivityIndicator
+  Image
 } from 'react-native'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5'
@@ -18,27 +17,30 @@ const todayDate = new Date()
 
 export default function FlightScheduleForm({}) {
   const { savedFlights, saveFlight } = useContext(RootContext)
-  const [airlines] = useState(JSON.parse(JSON.stringify(airlinesJSON)).data)
-  const [flightNumber, setFlightNumber] = useState('')
-  const [flightAirline, setFlightAirline] = useState('')
-  const [flightDate, setFlightDate] = useState(new Date())
-  const [isError, setIsError] = useState(false)
 
+  // INPUT STATE
+  const [inputFLNumber, setInputFLNumber] = useState('')
+  const [inputFLDate, setInputFLDate] = useState(new Date())
+  const [airlineCode, setAirlineCode] = useState('')
+
+  // VENDOR DATA
   const [flightAPIData, setFlightAPIData] = useState([])
-  const [finalData, setFinalData] = useState({
-    flightDetails: null
-  })
-  const [fetchStatus, setFetchStatus] = useState('idle')
+  const [airlines] = useState(JSON.parse(JSON.stringify(airlinesJSON)).data)
+
+  // FINAL DATA
+  const [finalFlightDetails, setFinalFlightDetails] = useState()
+  const [finalAirlineDetails, setFinalAirlineDetails] = useState()
+
+  // UTILITIES
+  const [isError, setIsError] = useState(false)
 
   useEffect(() => {
     const fetchFlightAPIData = async () => {
-      const stateDate = formatDate(flightDate.toDateString())
+      const stateDate = formatDate(inputFLDate.toDateString())
       const today = formatDate(todayDate.toDateString())
       const url = `https://www.hongkongairport.com/flightinfo-rest/rest/flights/past?date=${stateDate}&lang=en&cargo=false&arrival=false`
 
       try {
-        console.log('fetching')
-        setFetchStatus('fetching')
         const response = await fetch(url)
         const data = await response.json()
 
@@ -51,37 +53,32 @@ export default function FlightScheduleForm({}) {
           setFlightAPIData(data[0].list)
           matchingLocalFLNumberToFLAPIData(data[0].list)
         }
-
-        setTimeout(() => {
-          console.log('idle')
-          setFetchStatus('idle')
-        }, 800)
       } catch (error) {
         console.log(error)
       }
     }
 
     fetchFlightAPIData()
-  }, [flightDate])
+  }, [inputFLDate])
 
   useEffect(() => {
     matchingLocalFLNumberToFLAPIData(flightAPIData)
-  }, [flightNumber, flightDate])
+  }, [inputFLNumber, inputFLDate])
+
+  useEffect(() => {
+    matchingAirlineCodeToAirlinesAPI(airlineCode)
+  }, [airlineCode])
 
   const matchingLocalFLNumberToFLAPIData = data => {
-    setFinalData({
-      flightDetails: null
-    })
+    setFinalFlightDetails(null)
     const fetchedData = data
     console.log('matchingLocalFLNumberToFLAPIData', fetchedData)
     const matchedData = fetchedData.filter((item) => {
       item.flight.map((fl, index) => {
         /* console.log(index, fl) */
-        if (fl.no === flightNumber) {
-          setFinalData({
-            flightDetails: item
-          })
-          setFlightAirline(fl.airline)
+        if (fl.no === inputFLNumber) {
+          setFinalFlightDetails(item)
+          setAirlineCode(fl.airline)
         }
       })
     })
@@ -89,109 +86,44 @@ export default function FlightScheduleForm({}) {
     return matchedData
   }
 
-  const onChangeFlightNumber = flNumber => {
-    setFlightNumber(flNumber)
-    setFinalData({
-      flightDetails: null
+  const matchingAirlineCodeToAirlinesAPI = code => {
+    setFinalAirlineDetails(null)
+    const matchedData = airlines.filter(arl => {
+      return arl.icao_code == code
     })
+    setFinalAirlineDetails(matchedData)
   }
 
-  const onChangeFlightDate = (date) => {
-    setFlightDate(date)
+  const onChangeFlightNumberInput = flNumber => {
+    setInputFLNumber(flNumber)
+    setFinalFlightDetails(null)
+  }
+
+  const onChangeFlightDateInput = (date) => {
+    setInputFLDate(date)
   }
 
   const eraseAllStateValues = () => {
-    setFlightDate(new Date())
-    setFlightNumber('')
-  }
-
-  const matchLocalStateToTheAPI = async date => {
-    const url = `https://www.hongkongairport.com/flightinfo-rest/rest/flights/past?date=${formatDate(date.toDateString())}&lang=en&cargo=false&arrival=false`
-    let testDataMatched = []
-
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-
-      await data[0].list.filter((item) => {
-        item.flight.map((fl, index) => {
-          console.log(index, fl)
-          if (fl.no === flightNumber) {
-            testDataMatched.push(item)
-            setFlightAirline(fl.airline)
-          }
-        })
-      })
-
-      const airlineData = airlines.filter(arl => {
-        return arl.icao_code == flightAirline
-      })
-
-      console.log('getDataFromFlightAPI', data[0].list)
-      console.log('matchedData', testDataMatched)
-      console.log('airlineData', airlineData)
-
-      if (testDataMatched.length === 0) {
-        setIsError(true)
-      } else {
-        saveFlight({
-          ...testDataMatched[0],
-          airlineDetails: {
-            name: airlineData[0].airline_name,
-            country: airlineData[0].country_name,
-            fleetSize: airlineData[0].fleet_size,
-            fleetAverageAge: airlineData[0].fleet_average_age,
-            callSign: airlineData[0].callsign
-          },
-          flightNumber,
-          flightDate,
-          isActive: true
-        })
-      }
-    }
-    catch (err) {
-      return console.log(err)
-    }
+    setInputFLDate(new Date())
+    setInputFLNumber('')
   }
 
   const setReminder = () => {
-    matchLocalStateToTheAPI(flightDate)
-    eraseAllStateValues()
-  }
-
-  const renderModalResponse = status => {
-    switch (status) {
-      case 'idle':
-        return (
-          <Text style={styles.responseText}>Idle</Text>
-        )
-      case 'fetching':
-        return (
-          <>
-            <ActivityIndicator size="small" color="#3c40c6" />
-            <Text style={[styles.responseText, { marginTop: 20 }]}>Fetching data...</Text>
-          </>
-        )
-      default:
-        return (
-          <Text style={styles.responseText}>Idle</Text>
-        )
+    if (!finalFlightDetails) {
+      setIsError(true)
+    } else {
+      saveFlight({
+        flightNumber: inputFLNumber,
+        flightDate: inputFLDate,
+        ...finalFlightDetails,
+        airlineDetails: {...finalAirlineDetails[0]},
+        isActive: true
+      })
+      eraseAllStateValues()
     }
   }
 
-  const modalResponseVisibilityStatus = () => {
-    switch (fetchStatus) {
-      case 'idle':
-        return false
-      case 'fetching':
-        return true
-      default:
-        return false
-    }
-  }
-
-  console.log('finalData', finalData.flightDetails && finalData.flightDetails.flight)
-  console.log('airlineCode', flightAirline)
+  console.log(savedFlights)
 
   return (
     <View>
@@ -204,16 +136,16 @@ export default function FlightScheduleForm({}) {
         placeholder='Eg; Dec 20, 2020'
         label='Date'
         type='input-date'
-        inputValue={flightDate}
-        onChangeValue={onChangeFlightDate}
+        inputValue={inputFLDate}
+        onChangeValue={onChangeFlightDateInput}
       >
         <MaterialIcon name="date-range" size={15} color='#fff' />
       </Input>
       <Input
         placeholder='Eg; JA 8890'
         label='Flight Number'
-        inputValue={flightNumber}
-        onChangeValue={onChangeFlightNumber}
+        inputValue={inputFLNumber}
+        onChangeValue={onChangeFlightNumberInput}
         isDisabled={!flightAPIData}
       >
         <FontAwesomeIcon name="passport" size={15} color='#fff' />
@@ -223,16 +155,8 @@ export default function FlightScheduleForm({}) {
         isHaveIcon
         iconName='notifications'
         onPressButton={setReminder}
-        isDisabled={false}
+        isDisabled={!inputFLNumber ? true : false}
       />
-      <Modal
-        isModalVisible={modalResponseVisibilityStatus()}
-        isModalWithoutCloser
-      >
-        <View style={styles.responseModalContainer}>
-          {renderModalResponse(fetchStatus)}
-        </View>
-      </Modal>
       <Modal
         isModalVisible={isError}
         onPressClose={() => setIsError(false)}
@@ -244,9 +168,9 @@ export default function FlightScheduleForm({}) {
           }}
           source={require('../../../assets/404.gif')}
         />
-        <View style={{ paddingHorizontal: 10 }}>
+        <View style={{ paddingHorizontal: 20 }}>
           <Text style={styles.notFoundDialog}>
-            We're sorry, flight number {flightNumber} not found.
+            We're sorry, flight number {inputFLNumber} not found.
           </Text>
         </View>
       </Modal>
@@ -276,16 +200,5 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     fontSize: 14,
     textAlign: 'center'
-  },
-  responseModalContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginVertical: 'auto',
-    flex: 1
-  },
-  responseText: {
-    fontFamily: 'Poppins-Medium',
-    color: '#485460',
-    fontSize: 16
   }
 })
