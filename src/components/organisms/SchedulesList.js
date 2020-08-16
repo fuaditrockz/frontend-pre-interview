626dac248c02e5c5c56e1dfc2fb9cd3a0996fad7
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-16 21:58:58 +0700

    [ScheduleList] Update.

--- src/components/organisms/SchedulesList.js
+++ src/components/organisms/SchedulesList.js
@@ -10,7 +10,6 @@ import {
 import Empty from '../../../assets/icons/empty.svg'
 
 import { dates, amPmConvert } from '../../helpers'
-
 import { RootContext } from '../../context'
 import { FlightCard } from '../molecules'
 
@@ -21,18 +20,18 @@ export default function ScheduleList() {
   const { savedFlights, theme: { panelFullMode } } = useContext(RootContext)
   if (savedFlights.length === 0) {
     return (
-      <View style={{
-        width: '100%',
-        height: height - (panelFullMode ? 100 : (Platform.OS === 'ios' ? 60 : 90)),
-        paddingVertical: 30,
-        alignItems: 'center'
-      }}>
+      <View style={[
+        styles.emptyContainer,
+        {
+          height: height - (
+            panelFullMode ? 100 : (
+              Platform.OS === 'ios' ? 60 : 90
+            )
+          )
+        }
+      ]}>
         <Empty height={150} width={150} />
-        <Text style={{
-          fontFamily: 'Poppins-Bold',
-          fontSize: 16,
-          color: '#485460'
-        }}>
+        <Text style={styles.emptyText}>
           You don't have any set.
         </Text>
       </View>
@@ -61,5 +60,15 @@ const styles = StyleSheet.create({
   scrollView: {
     width: '100%',
     paddingHorizontal: 10
+  },
+  emptyContainer: {
+    width: '100%',
+    paddingVertical: 30,
+    alignItems: 'center'
+  },
+  emptyText: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 16,
+    color: '#485460'
   }
 })
