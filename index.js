6489a179816f6d64325c0deef59b12cbf362cb62
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-08-16 22:34:12 +0700

    [root] Set YellowBox for disable warning.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import { registerRootComponent } from 'expo';
+import { YellowBox } from 'react-native';
 import 'react-native-gesture-handler';
 
 import App from './src';
+
+YellowBox.ignoreWarnings([
+  'Non-serializable values were found in the navigation state',
+]);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
